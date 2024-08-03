7a333f81e51b176d66a109deeedb673388019666
Prasanna <pgurung1@my.hpu.edu>
2024-08-03 09:40:42 -0500

    Part 1 task complete

---
+++ moms-shopping-list-start/index.js
@@ -0,0 +1,18 @@
+
+const itemlist=document.getElementById("list");
+const form=document["addItem"]
+form.addEventListener("submit", (event)=>{
+ event.preventDefault();
+const listitem=document.createElement("li")
+const  item= document.createElement("div")
+item.textContent=form.title.value
+const editbtn=document.createElement("button")
+editbtn.textContent="edit"
+const xbtn=document.createElement("button")
+xbtn.textContent="X"
+listitem.appendChild(item)
+listitem.appendChild(editbtn)
+listitem.appendChild(xbtn)
+itemlist.append(listitem)
+form.title.value=""
+})
