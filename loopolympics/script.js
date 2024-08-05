60b2a6f0d17152c8c1c0a3b358ef85a706ec14f2
Prasanna <pgurung1@my.hpu.edu>
2024-08-05 15:16:13 -0500

    loop Olympics

---
+++ loopolympics/script.js
@@ -0,0 +1,104 @@
+// Preliminary
+// const fruit = ["banana", "orange", "apple", "kiwi"]
+
+// for(let i=0;i < fruit.length;i++)
+// {
+//     console.log(fruit[i])
+// }
+
+// for(let i=0; i <= 9; i++){
+//     console.log(i)
+// }
+
+// for(let i=9; i >= 0; i--){
+//     console.log(i)
+// }
+
+// Bronze
+arr=[]
+// for(let i=0; i <= 9; i++){
+//     arr.push(i)
+// }
+
+// for(let i=0; i <= 100; i++){
+//    if(i%2===0){
+//     console.log(i)
+//    }
+// }
+// const fruit = ["banana", "orange", "apple", "kiwi", "pear", "peach"]
+// for(let i=0;i < fruit.length;i++)
+// {
+//     if(i%2===0){
+//         arr.push(fruit[i])
+        
+//     }
+// }
+
+//Silver
+
+const people = [
+    {
+      name: "Harrison Ford",
+      occupation: "Actor"
+    },
+    {
+      name: "Justin Bieber",
+      occupation: "Singer"
+    },
+    {
+      name: "Vladimir Putin",
+      occupation: "Politician"
+    },
+    {
+      name: "Oprah",
+      occupation: "Entertainer"
+    }
+  ]
+  
+const names=[]
+const occupations=[]
+
+// for( let i=0;i < people.length;i++){
+//     console.log(people[i].name)
+// }
+
+// for( let i=0;i < people.length;i++){
+//     names.push(people[i].name)
+//     occupations.push(people[i].occupation)
+// }
+// console.log(names)
+// console.log(occupations)
+
+//Gold Medal
+
+let arr1=[]
+// let a="\n"
+// for( let i=0;i<3;i++)
+// {
+//     for(let j=0;j<3;j++){
+//        arr1.push(0)
+//     }
+//     arr.push(arr1)
+//     arr1=[]
+// }
+
+
+// for(let i=0; i < 3;i++)
+// {
+//     for(let j=0;j < 3; j++){
+//         arr1.push(i)
+//     }
+//     arr.push(arr1)
+//     arr1=[]
+// }
+// console.log(arr)
+
+// for(let i=0; i < 3;i++)
+// {
+//     for(let j=0;j < 3; j++){
+//         arr1.push(j)
+//     }
+//     arr.push(arr1)
+//     arr1=[]
+// }
+// console.log(arr)
