20252044d660bd26fef3d4973f5b08c09ad8ed7e
Prasanna <pgurung1@my.hpu.edu>
2024-08-24 18:12:38 -0500

    Settimer

--- level2/mariopestcontrol/script.js
+++ level2/mariopestcontrol/script.js
@@ -1,5 +1,7 @@
 
 const form=document.getElementById("pest-control")
+let count=0;
+let stopcount=false;
 form.addEventListener("submit", function(e){
     e.preventDefault();
     let bug1=document.getElementById("bug1").value *5
@@ -7,4 +9,30 @@ form.addEventListener("submit", function(e){
     let bug3=document.getElementById("bug3").value *11
     let total=document.getElementById("total")
     total.value=Number(bug1)+Number(bug2)+Number(bug3)
+
+    let display=document.getElementById("count")
+    display.textContent=count
+    localStorage.setItem("count",count)
+    setTimeout(() => {
+        console.log("hello")
+        console.log(count)
+        stopcount=true
+        
+    }, 10000);
+    if(!stopcount){
+        count++;
+    }
 })
+let displaytimer=document.getElementById("display-timer")
+
+function getTime(){
+    var now = new Date();
+    var sec= now.getSeconds();
+    if(sec < 10){
+        sec = `0${sec}`;
+    }
+
+    displaytimer.textContent = `:${sec}`;
+}
+
+getTime()
