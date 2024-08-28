e5104e04756312f078357a629197ad764ed91ee6
Prasanna <pgurung1@my.hpu.edu>
2024-08-28 17:51:46 -0500

    added lvl2 cssassignment

---
+++ level2/catchmeifyoucan/script.js
@@ -0,0 +1,26 @@
+// function sum(x, y){
+//     //check data types first and throw error
+//     if(typeof(x)!==Number || typeof(y)!==Number){
+//         throw "wrong type"
+//     }
+//         return x + y;
+    
+   
+//   }
+
+//  try{
+
+//    console.log( sum(1,2))
+//  }
+//  catch(err){
+//    console.log("Error")
+//  }
+
+
+
+
+  var user = {username: "sam", password: "123abc"};
+function login(username, password){
+  //check credentials
+}
+
