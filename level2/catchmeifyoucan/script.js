0705610e99130885f67c91e2ff6b4c9cd80031cd
Prasanna <pgurung1@my.hpu.edu>
2024-08-28 19:35:17 -0500

    modified catchmeifyoucan

--- level2/catchmeifyoucan/script.js
+++ level2/catchmeifyoucan/script.js
@@ -1,26 +1,37 @@
-// function sum(x, y){
-//     //check data types first and throw error
-//     if(typeof(x)!==Number || typeof(y)!==Number){
-//         throw "wrong type"
-//     }
-//         return x + y;
-    
-   
+function sum(x, y){
+    //check data types first and throw error
+    if(typeof(x)==="number" & typeof(y)==="number"){
+        return x + y;
+       
+    }else{
+       throw "wrong type"
+    }
+      
+     }
+
+ try{
+    console.log( sum("1",2))
+ }
+ catch(err){
+   console.log(err)
+ }
+
+
+
+
+//   var user = {username: "sam", password: "123abc"};
+// function login(username, password){
+//   if(username==="sam" & password==="123abc")
+//   {
+//     console.log("login successfull")
+//   }else{
+//     throw "Wrong username or password"
 //   }
+// }
 
-//  try{
-
-//    console.log( sum(1,2))
-//  }
-//  catch(err){
-//    console.log("Error")
-//  }
-
-
-
-
-  var user = {username: "sam", password: "123abc"};
-function login(username, password){
-  //check credentials
-}
-
+// try{
+//    login("sa","123abc")
+// }
+// catch(err){
+//     console.log("error")
+// }
